bd7963108d2386c801c17da0a89b6d8db2c26a88
ManKotzent <137501602+ManKotzent@users.noreply.github.com>
2025-04-08 16:49:48 +0200

    Added TODO to ActionCardActivity.kt

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/ActionCardActivity.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/ActionCardActivity.kt
@@ -7,23 +7,40 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import at.aau.serg.websocketbrokerdemo.network.MyStomp
+import com.google.gson.Gson
 
-class ActionCardActivity(private var actionCard: ActionCard) : ComponentActivity() {
+class ActionCardActivity : ComponentActivity(), Callbacks {
 
-    @SuppressLint("DiscouragedApi")
+    private lateinit var stomp: MyStomp
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_action_card)
 
-        findViewById<TextView>(R.id.headline).text = actionCard.headline
+        val playerName = intent.getStringExtra("playerName") ?: "Spieler"
 
-        val resId = resources.getIdentifier(actionCard.imageName, "drawable", applicationContext.packageName)
-        if (resId != 0) findViewById<ImageView>(R.id.picture).setImageResource(resId)
-
-        findViewById<TextView>(R.id.description).text = actionCard.action
+        stomp = MyStomp(this)
+        stomp.sendMove(playerName, "zieht Action Card")
 
         findViewById<Button?>(R.id.button).setOnClickListener {
             Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
+            //TODO: Switch to different view
+        }
+    }
+
+    @SuppressLint("DiscouragedApi")
+    override fun onResponse(res: String) {
+        runOnUiThread {
+            val gson = Gson()
+            val card = gson.fromJson(res, ActionCard::class.java)
+
+            findViewById<TextView>(R.id.headline).text = card.headline
+
+            val resId = resources.getIdentifier(card.imageName, "drawable", applicationContext.packageName)
+            if (resId != 0) findViewById<ImageView>(R.id.picture).setImageResource(resId)
+
+            findViewById<TextView>(R.id.description).text = card.action
         }
     }
 }
