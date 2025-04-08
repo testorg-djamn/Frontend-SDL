99c2ea930e4338ec629dd2ba942753b9b7160160
Andreas Zarfl <anzarf@edu.aau.at>
2025-04-08 23:28:09 +0200

    UI excluded from sonarclaud

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,7 +113,7 @@ sonar {
         property("sonar.host.url", "https://sonarcloud.io")
         property("sonar.java.coveragePlugin", "jacoco")
         property("sonar.coverage.jacoco.xmlReportPaths", "${layout.buildDirectory.get()}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
-        property("sonar.exclusions", "**/SettingsActivity.kt,**/StartActivity.kt,**/MainActivity.kt,**/MyStomp.kt")
+        property("sonar.exclusions", "**/SettingsActivity.kt,**/StartActivity.kt,**/MainActivity.kt,**/MyStomp.kt,**/Color.kt,**/Theme.kt,**/Type")
     }
 }
 
