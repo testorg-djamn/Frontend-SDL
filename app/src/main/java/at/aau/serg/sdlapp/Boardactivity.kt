d3a29ad69501ce3fbe2b49694dc2634d0f32a5e5
Andreas Zarfl <anzarf@edu.aau.at>
2025-04-15 12:07:57 +0200

    improved board

--- app/src/main/java/at/aau/serg/sdlapp/Boardactivity.kt
+++ app/src/main/java/at/aau/serg/sdlapp/Boardactivity.kt
@@ -1,42 +1,44 @@
 package at.aau.serg.sdlapp
 
-    import android.os.Bundle
-    import android.view.View
-    import androidx.activity.ComponentActivity
-    import androidx.core.view.WindowCompat
-    import androidx.core.view.WindowInsetsCompat
-    import androidx.core.view.WindowInsetsControllerCompat
-    import com.otaliastudios.zoom.ZoomLayout
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.otaliastudios.zoom.ZoomLayout
 
-    class BoardActivity : ComponentActivity() {
+class BoardActivity : ComponentActivity() {
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_board)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board)
 
-            // Vollbildmodus aktivieren (mit stabilerer Implementierung)
-            enableFullscreen()
+        // Vollbildmodus aktivieren (mit stabilerer Implementierung)
+        enableFullscreen()
 
-            val zoomLayout = findViewById<ZoomLayout>(R.id.zoomLayout)
+        val zoomLayout = findViewById<ZoomLayout>(R.id.zoomLayout)
 
-            // ZoomLayout-Einstellungen anpassen
-            zoomLayout.setMaxZoom(3.0f)
-            zoomLayout.setMinZoom(0.7f)
-            zoomLayout.setZoomEnabled(true)
-            zoomLayout.setHorizontalPanEnabled(true)
-            zoomLayout.setVerticalPanEnabled(true)
-        }
+// Zoom über die Engine setzen
+        zoomLayout.zoomTo(3.0f, false)
 
-        private fun enableFullscreen() {
-            // Verwende die Jetpack-Bibliothek für eine konsistentere Implementierung
-            val windowInsetsController = WindowInsetsControllerCompat(window, window.decorView)
-
-            // Verstecke Statusleiste und Navigationsleiste
-            windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
-            windowInsetsController.systemBarsBehavior =
-                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-
-            // Setze den Content im Vollbildmodus
-            WindowCompat.setDecorFitsSystemWindows(window, false)
-        }
+// ZoomLayout-Einstellungen mit Java-Syntax anpassen
+        zoomLayout.setMaxZoom(4.0f)
+        zoomLayout.setMinZoom(1.0f)
+        zoomLayout.setZoomEnabled(true)
+        zoomLayout.setHorizontalPanEnabled(true)
+        zoomLayout.setVerticalPanEnabled(true)
     }
+
+    private fun enableFullscreen() {
+        // Verwende die Jetpack-Bibliothek für eine konsistentere Implementierung
+        val windowInsetsController = WindowInsetsControllerCompat(window, window.decorView)
+
+        // Verstecke Statusleiste und Navigationsleiste
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        // Setze den Content im Vollbildmodus
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
