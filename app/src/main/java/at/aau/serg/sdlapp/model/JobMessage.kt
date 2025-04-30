1d6f45ec22e42b5c90adde333f2baa4f14542bce
Andre Salzer <ansalzer@edu.aau.at>
2025-04-30 23:38:24 +0200

    Added Feature that a player with a job gets this one shown on the left popup side

--- app/src/main/java/at/aau/serg/sdlapp/model/JobMessage.kt
+++ app/src/main/java/at/aau/serg/sdlapp/model/JobMessage.kt
@@ -1,13 +1,15 @@
 package at.aau.serg.sdlapp.model
 
+import com.google.gson.annotations.SerializedName
+
 data class JobMessage(
-    val jobId: Int,
-    val title: String,
-    val salary: Int,
-    val bonusSalary: Int,
-    val requiresHighSchoolDiploma: Boolean,
-    val isTaken: Boolean,
-    val takenByPlayerName: String?,  // Nullable if not taken
-    val playerName: String,
-    val timestamp: String
+    @SerializedName("jobId") val jobId: Int,
+    @SerializedName("title") val title: String,
+    @SerializedName("salary") val salary: Int,
+    @SerializedName("bonusSalary") val bonusSalary: Int,
+    @SerializedName("requiresDegree") val requiresDegree: Boolean,
+    @SerializedName("isTaken") val isTaken: Boolean,
+    @SerializedName("assignedToPlayerName") val assignedToPlayerName: String?,
+    @SerializedName("playerName") val playerName: String,
+    @SerializedName("timestamp") val timestamp: String
 )
