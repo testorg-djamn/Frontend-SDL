9e17ed08b78f87309b9a684357ac865b9667bee9
Andreas Zarfl <anzarf@edu.aau.at>
2025-05-06 12:47:03 +0200

    fix(build): Enable code obfuscation in release build for security compliance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,148 +1,147 @@
 plugins {
-      alias(libs.plugins.android.application)
-      alias(libs.plugins.kotlin.android)
-      alias(libs.plugins.kotlin.compose)
-      id("jacoco")
-      id("org.sonarqube") version "5.1.0.4882"
-  }
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("jacoco")
+    id("org.sonarqube") version "5.1.0.4882"
+}
 
-  android {
-      namespace = "at.aau.serg.sdlapp"
-      compileSdk = 35
+android {
+    namespace = "at.aau.serg.sdlapp"
+    compileSdk = 35
 
-      defaultConfig {
-          applicationId = "at.aau.serg.sdlapp"
-          minSdk = 30
-          targetSdk = 35
-          versionCode = 1
-          versionName = "1.0"
+    defaultConfig {
+        applicationId = "at.aau.serg.sdlapp"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
 
-          testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-      }
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
 
-      buildTypes {
-          release {
-              isMinifyEnabled = false
-              proguardFiles(
-                  getDefaultProguardFile("proguard-android-optimize.txt"),
-                  "proguard-rules.pro"
-              )
-          }
-      }
-      compileOptions {
-          sourceCompatibility = JavaVersion.VERSION_11
-          targetCompatibility = JavaVersion.VERSION_11
-      }
-      kotlinOptions {
-          jvmTarget = "11"
-      }
-      buildFeatures {
-          compose = true
-          viewBinding = true
-      }
+    buildTypes {
+        release {
+            isMinifyEnabled = true 
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
 
-      testOptions {
-          unitTests {
-              all {
-                  it.useJUnitPlatform()
-                  it.finalizedBy(tasks.named("jacocoTestReport"))
-              }
-          }
-      }
-  }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+        viewBinding = true
+    }
 
-  tasks.register<JacocoReport>("jacocoTestReport") {
-      group = "verification"
-      description = "Generates code coverage report for the test task."
-      dependsOn("testDebugUnitTest")
+    testOptions {
+        unitTests {
+            all {
+                it.useJUnitPlatform()
+                it.finalizedBy(tasks.named("jacocoTestReport"))
+            }
+        }
+    }
+}
 
-      reports {
-          xml.required.set(true)
-          xml.outputLocation.set(file("${project.projectDir}/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"))
-      }
+tasks.register<JacocoReport>("jacocoTestReport") {
+    group = "verification"
+    description = "Generates code coverage report for the test task."
+    dependsOn("testDebugUnitTest")
 
-      val fileFilter = listOf(
-          "**/R.class",
-          "**/R$*.class",
-          "**/BuildConfig.*",
-          "**/Manifest*.*",
-          "**/*Test*.*",
-          "android/**/*.*"
-      )
+    reports {
+        xml.required.set(true)
+        xml.outputLocation.set(file("${project.projectDir}/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"))
+    }
 
-      val debugTree =
-          fileTree("${project.layout.buildDirectory.get().asFile}/tmp/kotlin-classes/debug") {
-              exclude(fileFilter)
-          }
+    val fileFilter = listOf(
+        "**/R.class",
+        "**/R$*.class",
+        "**/BuildConfig.*",
+        "**/Manifest*.*",
+        "**/*Test*.*",
+        "android/**/*.*"
+    )
 
-      val javaDebugTree =
-          fileTree("${project.layout.buildDirectory.get().asFile}/intermediates/javac/debug") {
-              exclude(fileFilter)
-          }
+    val debugTree = fileTree("${project.layout.buildDirectory.get().asFile}/tmp/kotlin-classes/debug") {
+        exclude(fileFilter)
+    }
 
-      val mainSrc = listOf(
-          "${project.projectDir}/src/main/java",
-          "${project.projectDir}/src/main/kotlin"
-      )
+    val javaDebugTree = fileTree("${project.layout.buildDirectory.get().asFile}/intermediates/javac/debug") {
+        exclude(fileFilter)
+    }
 
-      sourceDirectories.setFrom(files(mainSrc))
-      classDirectories.setFrom(files(debugTree, javaDebugTree))
-      executionData.setFrom(fileTree(project.layout.buildDirectory.get().asFile) {
-          include("jacoco/testDebugUnitTest.exec")
-          include("outputs/unit_test_code_coverage/debugUnitTest/testDebugUnitTest.exec")
-      })
-  }
+    val mainSrc = listOf(
+        "${project.projectDir}/src/main/java",
+        "${project.projectDir}/src/main/kotlin"
+    )
 
-  sonar {
-      properties {
-          property("sonar.exclusions", "**/WheelActivity.kt")
-          property("sonar.projectKey", "SE2-SS25-SpielDesLebens_Frontend-SDL")
-          property("sonar.organization", "se2-ss25-spieldeslebens")
-          property("sonar.host.url", "https://sonarcloud.io")
-          property("sonar.java.coveragePlugin", "jacoco")
-          property("sonar.coverage.jacoco.xmlReportPaths", "${layout.buildDirectory.get()}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
-          property("sonar.exclusions", "**/*Activity.kt,**/MyStomp.kt,**/Color.kt,**/Theme.kt,**/Type.kt,**/ActionCard.kt,**/PlayerModell.kt,**/PlayerRepository.kt,**/PlayerStatsOverlay.kt,**/GameScreen.kt,**/BoardData.kt,**/Field.kt,**/FieldTyp.kt,**/Board.kt")
-      }
-  }
+    sourceDirectories.setFrom(files(mainSrc))
+    classDirectories.setFrom(files(debugTree, javaDebugTree))
+    executionData.setFrom(fileTree(project.layout.buildDirectory.get().asFile) {
+        include("jacoco/testDebugUnitTest.exec")
+        include("outputs/unit_test_code_coverage/debugUnitTest/testDebugUnitTest.exec")
+    })
+}
 
-  dependencies {
-      // App
-      implementation(libs.krossbow.websocket.okhttp)
-      implementation(libs.krossbow.stomp.core)
-      implementation(libs.krossbow.websocket.builtin)
-      implementation(libs.androidx.core.ktx)
-      implementation(libs.androidx.lifecycle.runtime.ktx)
-      implementation(libs.androidx.activity.compose)
-      implementation(platform(libs.androidx.compose.bom))
-      implementation(libs.androidx.ui)
-      implementation(libs.androidx.ui.graphics)
-      implementation(libs.androidx.ui.tooling.preview)
-      implementation(libs.androidx.material3)
-      implementation(libs.androidx.constraintlayout)
-      implementation(libs.androidx.recyclerview)
-      implementation(libs.gson)
-      implementation(libs.zoomlayout)
+sonar {
+    properties {
+        property("sonar.exclusions", "**/WheelActivity.kt")
+        property("sonar.projectKey", "SE2-SS25-SpielDesLebens_Frontend-SDL")
+        property("sonar.organization", "se2-ss25-spieldeslebens")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.java.coveragePlugin", "jacoco")
+        property("sonar.coverage.jacoco.xmlReportPaths", "${layout.buildDirectory.get()}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
+        property("sonar.exclusions", "**/*Activity.kt,**/MyStomp.kt,**/Color.kt,**/Theme.kt,**/Type.kt,**/ActionCard.kt,**/PlayerModell.kt,**/PlayerRepository.kt,**/PlayerStatsOverlay.kt,**/GameScreen.kt,**/BoardData.kt,**/Field.kt,**/FieldTyp.kt,**/Board.kt")
+    }
+}
 
-      // Unit-Tests
-      testImplementation(libs.junit)
-      testImplementation(libs.junit.jupiter.api)
-      testRuntimeOnly(libs.junit.jupiter.engine)
-      testImplementation(libs.mockito.core)
-      testImplementation(libs.robolectric)
-      testImplementation(libs.androidx.test.core)
-      testImplementation(libs.androidx.test.core.ktx)
-      testImplementation(libs.androidx.test.ext.junit)
-      testImplementation(libs.androidx.arch.core.testing)
+dependencies {
+    // App
+    implementation(libs.krossbow.websocket.okhttp)
+    implementation(libs.krossbow.stomp.core)
+    implementation(libs.krossbow.websocket.builtin)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.gson)
+    implementation(libs.zoomlayout)
 
-      // Instrumentation Tests (Espresso + Intents)
-      androidTestImplementation(libs.androidx.test.runner)
-      androidTestImplementation(libs.androidx.test.ext.junit)
-      androidTestImplementation(libs.androidx.espresso.core)
-      androidTestImplementation(libs.androidx.espresso.intents)
+    // Unit-Tests
+    testImplementation(libs.junit)
+    testImplementation(libs.junit.jupiter.api)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.androidx.test.core)
+    testImplementation(libs.androidx.test.core.ktx)
+    testImplementation(libs.androidx.test.ext.junit)
+    testImplementation(libs.androidx.arch.core.testing)
 
-      // Compose Test
-      androidTestImplementation(platform(libs.androidx.compose.bom))
-      androidTestImplementation(libs.androidx.ui.test.junit4)
-      debugImplementation(libs.androidx.ui.tooling)
-      debugImplementation(libs.androidx.ui.test.manifest)
-  }
+    // Instrumentation Tests (Espresso + Intents)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.espresso.intents)
+
+    // Compose Test
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
